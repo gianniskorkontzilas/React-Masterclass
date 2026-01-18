12e598420a8909e0c6e67c6d407d6579e54b72e6
gianniskorkontzilas <gianniskorkontzilas@hotmail.com>
2026-01-19 00:00:13 +0200

    Components Hierarchy

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import ParentComp from './components/ParentComp'
+
+
+function App() {
+
+  return (
+    <div>
+      <ParentComp></ParentComp>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/ParentComp.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ChildComp from "./ChildComp";
+
+const ParentComp = () => {
+    return (
+        <div>
+            Parent Component is here
+           <ChildComp></ChildComp>
+        </div>
+    )
+}
+
+export default ParentComp
