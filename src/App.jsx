639bd9c12aa560a422f4066d943f75672e0ef3c4
gianniskorkontzilas <gianniskorkontzilas@hotmail.com>
2026-01-19 00:09:25 +0200

    Create a Class Component

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import ParentComp from './components/ParentComp'
-
+import ClassComp from './components/ClassComp'
 
 function App() {
 
   return (
     <div>
-      <ParentComp></ParentComp>
+      <ClassComp></ClassComp>
     </div>
   )
 }
---
+++ src/components/ClassComp.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+class ClassComp extends React.Component {
+
+    render() {
+        return(
+            <div>
+                This is a class component
+            </div>
+        )
+    }
+}
+
+export default ClassComp
