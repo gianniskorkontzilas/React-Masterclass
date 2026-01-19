678b8d5cc18b49362257622241c2ee310f4d51f9
gianniskorkontzilas <gianniskorkontzilas@hotmail.com>
2026-01-19 18:16:43 +0200

    State management with useState Hook

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,28 @@ import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import StateLessComponent from './components/StateLessComponent'
-import Stateful from './components/Stateful'
+
 
 function App() {
 
-const [count, setCount]  = useState(0)
+const [stringState, setStingState] = useState("hello")
+const [count, setCount] = useState(1)
 
   return (
     <div>
-      <p>count number:  {count}</p>
-      <button onClick={() => {
-        setCount(prev => prev + 1)
-      }}>Increas count</button>
+      this is count: <b>{count}</b>
+      <br/>
+      <button onClick={() =>{
+       setCount(previous => previous + 1)
+      }}>change count</button>
+      <br/>
+      <br/>
+      this is the text: <b>{stringState}</b>
+      <br/>
+      <button onClick={() =>{
+        setStingState("world")
+      }}>change string</button>
+    
     </div>
   )
 }
