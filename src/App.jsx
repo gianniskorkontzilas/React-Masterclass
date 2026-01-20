cf6215c07ebef0d25dca39f6df35b6503efc71d1
gianniskorkontzilas <gianniskorkontzilas@hotmail.com>
2026-01-20 19:38:07 +0200

    Optional Chaining

--- src/App.jsx
+++ src/App.jsx
@@ -3,48 +3,24 @@ import './App.css'
 
 function App() {
 
-  let jsxSlots = {
-    slot1: 'slot1',
-    slot2: 'slot2',
-    slot3: 'slot3'
-  }
+  const [array, setArray] = useState([
+    {key: 1},
+    {key: undefined},
+    {key: 3}
+  ])
 
-  const [currentSlot, setCurrentSlot] = useState(jsxSlots.slot1)
-
-  if(currentSlot === jsxSlots.slot1) {
-    return (
-      <div className='container'>
-        <div>Slot 1</div>
-        <button onClick={() => setCurrentSlot (jsxSlots.slot2)}>go to slot2</button>
-      </div>
-    )
-  }
-
-  if(currentSlot === jsxSlots.slot2) {
-    return (
-      <div className='container'>
-        <div>Slot 2</div>
-        <button onClick={() => setCurrentSlot (jsxSlots.slot3)}>go to slot 3</button>
-      </div>
-    )
-  }
-
-  if(currentSlot === jsxSlots.slot3) {
-    return (
-      <div className='container'>
-        <div>Slot 3</div>
-        <button onClick={() => setCurrentSlot ("doesnt exist")}> go to default slot</button>
-      </div>
-    )
-  }
+  return (
+    <div>
+      {
+        array?.map((curr, index) => {
+          return (<p key={index}>{curr?.key}</p>)
+        })
+      }
+    </div>
 
 
+  )
 
- return (
-  <div className="container">
-    hello
-  </div>
- )
 }
 
  export default App
