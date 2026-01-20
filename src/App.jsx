f4944bc82cc742d8f6d768f8a7e3a94083a16928
gianniskorkontzilas <gianniskorkontzilas@hotmail.com>
2026-01-20 13:49:14 +0200

    Inline Css Style React

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,18 @@
-import React, { useReducer } from 'react'
+import React from "react"
 import './App.css'
-
-
 
 function App() {
 
-  const initialState = {
-    count: 0,
-    text: '',
 
-  }
-
-  const reducerFunction = (state, action) => {
-    if(action.type === 'increment') {
-      return {
-        ...state,
-        count: state.count + 1,
-      }
-    }
-    if(action.type === 'decrement') {
-      return {
-        ...state,
-        count:state.count - 1,
-      }
-    }
-    if(action.type === 'setText') {
-      return {
-        ...state,
-        text: action.payload,
-      }
-    }
-
-    return state;
-
-  }
-
-  const [state, dispatch] = useReducer(reducerFunction,initialState);
-
-  return (
-    <div className='...'>
-      Count: {state.count}
-      <br/>
-      Text: {state.text}
-      <br/>
-      <button onClick={() => dispatch({type: 'increment'})}>Increase count</button>
-      <br/>
-      <button onClick={() => dispatch({type: 'decrement'})}>Decrease count</button>
-      <br/>
-      <button onClick={() => dispatch({type: 'setText', payload: 'New Text'})}>Change Text</button>
+  return(
+    <div>
+      <h1 style={{
+        color: 'blue',
+        fontSize: '5rem'
+      }}>React Master</h1>
     </div>
   )
 }
-
 
 
 export default App
