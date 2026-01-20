bc5d510d560aedb2257995abc36f6375a95f680b
gianniskorkontzilas <gianniskorkontzilas@hotmail.com>
2026-01-20 18:10:11 +0200

    Toggle enable and Disable

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,37 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import './App.css'
 
 function App() {
 
- 
-  
 
-  
-  return(
-    <>
-      <div>React</div>
-      <div>MasterClass</div>
-    </>
-  )
+  const [value, setValue] = useState('')
+  const [disabled, setDisabled] = useState(true)
+
+  useEffect(() => {
+    if(value.length > 0) {
+      setDisabled(false);
+    } else {
+      setDisabled(true);
+    }
+
+  }, [value])
+
+ return (
+  <div className="container">
+    <input value={value} onChange={(e) =>{
+      setValue(e.target.value);
+    }} type="text" />
+    <button
+    disabled={disabled}
+    onClick={() => { 
+      alert("The input value is: " + value)
+
+    }}> 
+      Show Value
+    </button>
+
+  </div>
+ )
 }
 
-export default App
+ export default App
