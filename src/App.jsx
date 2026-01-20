36fa69063e04ca4009f53254ca8dc870a03076bd
gianniskorkontzilas <gianniskorkontzilas@hotmail.com>
2026-01-20 16:43:30 +0200

    Ternary Operator in UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,22 @@
-import React from "react"
+import React, { useState } from "react"
 import './App.css'
 
 function App() {
 
-  let list = [
-    {
-      id:1,
-      name: 'John Doe',
-    },
-    {
-      id:2,
-      name: 'Jane Doe',
-    },
-    {
-      id:3,
-      name: 'John Smith',
-    },
-    {
-      id:4,
-      name: 'Jane Smith',
-    },
-  ]
  
-  // const list2 = list.filter(curr => curr.id !==3) 
-  
+  const [condition, setCondition] = useState(false)
   
   return(
     <div className="...">
     {
-      list.filter(curr => curr.id !== 3).map(curr => {
-        return(
-          <p key={curr.id}>{curr.id}: {curr.name}</p>
-        )
-      })
+      condition?
+      <p style={{color: 'green'}}>
+        The condition is true
+      </p>
+      :
+      <p style={{color: 'red'}}>
+      The condition is false
+    </p>
     }
     </div>
   )
