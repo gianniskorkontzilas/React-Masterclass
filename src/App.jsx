adbb0888d1392bb593b2ab0d072232e0795f1f29
gianniskorkontzilas <gianniskorkontzilas@hotmail.com>
2026-01-20 18:54:48 +0200

    conditional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -3,41 +3,43 @@ import './App.css'
 
 function App() {
 
+  let jsxSlots = {
+    slot1: 'slot1',
+    slot2: 'slot2',
+    slot3: 'slot3'
+  }
 
-  const [value, setValue] = useState('')
-  const [disabled, setDisabled] = useState(true)
+  const [currentSlot, setCurrentSlot] = useState(jsxSlots.slot4)
 
-  useEffect(() => {
-    if(value.length > 0) {
-      setDisabled(false);
-    } else {
-      setDisabled(true);
-    }
+  if(currentSlot === jsxSlots.slot1) {
+    return (
+      <div className='container'>
+        <div>Slot 1</div>
+      </div>
+    )
+  }
 
-  }, [value])
+  if(currentSlot === jsxSlots.slot2) {
+    return (
+      <div className='container'>
+        <div>Slot 2</div>
+      </div>
+    )
+  }
+
+  if(currentSlot === jsxSlots.slot3) {
+    return (
+      <div className='container'>
+        <div>Slot 3</div>
+      </div>
+    )
+  }
+
+
 
  return (
   <div className="container">
-    <input value={value} 
-    onKeyUp={(event) => {
-      if(event.key === 'Enter') {
-        alert("Input value is submitted: " + value);
-      }
-
-    }}
-    onChange={(e) =>{
-      setValue(e.target.value);
-    }} type="text" />
-
-    <button
-    disabled={disabled}
-    onClick={() => { 
-      alert("The input value is: " + value)
-
-    }}> 
-      Show Value
-    </button>
-
+    hello
   </div>
  )
 }
