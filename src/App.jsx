a02aed39e9627aad7feb979f73a05df9cbee86d5
gianniskorkontzilas <gianniskorkontzilas@hotmail.com>
2026-01-20 17:06:43 +0200

    React Fragment

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,17 @@
-import React, { useState } from "react"
+import React from "react"
 import './App.css'
 
 function App() {
 
  
-  const [condition, setCondition] = useState(false)
-  const [condition2, setCondition2] = useState(false)
+  
 
   
   return(
-    <div className="...">
-    {
-      condition?
-      <p style={{color: 'green'}}>
-        The condition is true
-      </p>
-      :
-      condition2 ?
-      <p style={{color: 'green'}}>
-      The condition one is false and condition2 is true
-    </p> :
-     <p style={{color: 'red'}}>
-     The condition one is false and condition2 is also false
-   </p>
-    }
-    </div>
+    <>
+      <div>React</div>
+      <div>MasterClass</div>
+    </>
   )
 }
 
