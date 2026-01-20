078f6ce3d702de0bb8e2be624e566873256b20e2
gianniskorkontzilas <gianniskorkontzilas@hotmail.com>
2026-01-20 14:47:23 +0200

    Map method ex2

--- src/App.jsx
+++ src/App.jsx
@@ -5,35 +5,21 @@ function App() {
 
   let array = ['Hello', 'I', 'am', 'a', 'React', 'Developer']
  
-
   return(
-    <div className="container">
+    <div className="...">
       {
         array.map((curr) => {
-
           if(curr === 'React') {
-            return (
-              <p style={{color: 'blue'}}>{curr}</p>
-            )
-          }
-
-          if(curr.length <= 3) {
-            return (
-              <p style={{
-                color: 'red', 
-              }}>{curr}</p>
-            )
+            return 'Awesome'
           }
 
           return (
             <p>{curr}</p>
           )
-        }
-      )
+        } )
       }
     </div>
   )
 }
-
 
 export default App
