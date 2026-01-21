af838eaf3d484e589813a8c61d97192a696f2c79
gianniskorkontzilas <gianniskorkontzilas@hotmail.com>
2026-01-21 04:12:43 +0200

    React Router And Error404 Page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Home from "./pages/Home"
 import About from "./pages/About"
+import Error404 from "./pages/Error404"
 
 function App() {
 
@@ -15,6 +16,7 @@ function App() {
       <Routes>
         <Route element={<Home />} path='/' />
         <Route element={<About />} path='/about'/>
+        <Route element={<Error404 />} path='*'/>
       </Routes>
     </BrowserRouter>
 
