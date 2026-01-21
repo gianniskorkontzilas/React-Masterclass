98ac3bd7fcbbc69adcf688c9c4dae04b83dfa92a
gianniskorkontzilas <gianniskorkontzilas@hotmail.com>
2026-01-21 04:02:22 +0200

    React Router With Link

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,14 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 
 function Home() {
     return (
-        <div>Home</div>
+        <div>
+            <div>Home</div>
+            <Link to='/about'>
+                Go to About page
+            </Link>
+        </div>
     )
 }
 
